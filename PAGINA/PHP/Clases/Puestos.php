7a371f4e95fd42e3f19d94ec45a2f842e772848a
DanySapiens <dany_sapiens@live.com.mx>
2023-07-18 15:36:17 -0700

    se crea funcion para consultar puestos activos

--- PAGINA/PHP/Clases/Puestos.php
+++ PAGINA/PHP/Clases/Puestos.php
@@ -3,10 +3,10 @@ include_once ("conexion.php");
 
 class Puestos{
     
-    public static function consultaPuestoActivo($opcion,$id){
+    public static function consultaPuestoActivo($opcion,$idpuesto){
         $conexion = new Conexion();
         $conexion -> conectar();
-        $query = "select * from fnoperacionespuestos({$opcion},{$id},0,'');";
+        $query = "select * from fnoperacionespuestos({$opcion},{$idpuesto},'',0,0);";
         $resultado = $conexion -> ejecutarConsulta($query);
         $data = array();
         foreach($resultado as $row){
